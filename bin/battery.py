ce190620bd574dc86634a5d6f3417ec4d4e3504b
yohlane <yohlane@gmail.com>
2017-10-09 11:25:08 +0200

    Battery fix after update

--- bin/battery.py
+++ bin/battery.py
@@ -18,18 +18,28 @@ else:
     # available for each battery separately, although state and remaining 
     # time for overall block is shown in the status of the first battery 
     batteries = status.split("\n")
-    state_batteries=[]
-    commasplitstatus_batteries=[]
-    percentleft_batteries=[]
+    percentleft=0
+    state=''
+    batterie_max=0
+
     for battery in batteries:
-        if battery!='':
-            state_batteries.append(battery.split(": ")[1].split(", ")[0])
-            commasplitstatus = battery.split(", ")
-            percentleft_batteries.append(int(commasplitstatus[1].rstrip("%\n")))
-            commasplitstatus_batteries.append(commasplitstatus)
-    state = state_batteries[0]
-    commasplitstatus = commasplitstatus_batteries[0]
-    percentleft = int(sum(percentleft_batteries)/len(percentleft_batteries))
+      if battery:
+        commasplitstatus = battery.split(", ")
+        charge = int(commasplitstatus[1].rstrip("%\n"))
+        if charge > batterie_max:
+          batterie_max=charge
+          percentleft=charge
+          state = battery.split(": ")[1].split(", ")[0]
+
+    #for battery in batteries:
+    #    if battery!='':
+    #        state_batteries.append(battery.split(": ")[1].split(", ")[0])
+    #        commasplitstatus = battery.split(", ")
+    #        percentleft_batteries.append(int(commasplitstatus[1].rstrip("%\n")))
+    #        commasplitstatus_batteries.append(commasplitstatus)
+    #state = state_batteries[0]
+    #commasplitstatus = commasplitstatus_batteries[0]
+    #percentleft = int(sum(percentleft_batteries)/len(percentleft_batteries))
     # stands for charging
     FA_LIGHTNING = "<span color='yellow'><span font='FontAwesome'>\uf0e7</span></span>"
 
